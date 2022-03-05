b9badecab71a13565e8014e5ff1fecaa2530b368
Dan Santos <dados.dan.santos@gmail.com>
2022-03-05 16:09:45 -0300

    feat: make bell ring

---
+++ client/index.js
@@ -0,0 +1,46 @@
+document.body.onload = function () {
+    const actionButton = document.querySelector('#actionButton');
+    const audio = document.querySelector('#audio');
+
+    const ringBell = function () {
+        audio.play();        
+        const buttonsElements = actionButton.querySelectorAll('span');
+        const iconElement = buttonsElements[1].querySelector('i');
+
+        // actionButton.removeEventListener('click', function(){});
+
+        actionButton.classList.remove('is-primary');
+        actionButton.classList.add('is-danger');
+        actionButton.classList.remove('play');
+        actionButton.classList.add('stop');
+        buttonsElements[0].innerHTML = 'Parar';
+        iconElement.classList.remove('fa-bell');
+        iconElement.classList.add('fa-pause');
+    }
+
+    const stopBell = function() {
+        audio.pause();
+        audio.load();
+        const buttonsElements = actionButton.querySelectorAll('span');
+        const iconElement = buttonsElements[1].querySelector('i');
+
+        // actionButton.removeEventListener('click', function(){});
+
+        actionButton.classList.remove('is-danger');
+        actionButton.classList.add('is-primary');        
+        actionButton.classList.remove('stop');
+        actionButton.classList.add('play');        
+        buttonsElements[0].innerHTML = 'Tocar';
+        iconElement.classList.remove('fa-pause');
+        iconElement.classList.add('fa-bell');
+        
+    }
+
+    actionButton.addEventListener('click', function() {
+        if (this.className.includes('play')) {
+            ringBell();
+        } else {
+            stopBell();
+        }
+    });
+}
